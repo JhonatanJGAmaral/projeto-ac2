150e4442fc32ed506f7d0ec70f8cbca49e7a2413
Eduardo Almeida <contato.eduardopex@hotmail.com>
2023-06-13 22:28:47 -0300

    chore: updated jest config validation.

--- jest.config.ts
+++ jest.config.ts
@@ -13,9 +13,9 @@ const config: Config = {
   roots: ["<rootDir>/src/", "<rootDir>/test/"],
   coverageThreshold: {
     global: {
-      branches: 40,
-      functions: 71,
-      lines: 71,
+      branches: 70,
+      functions: 70,
+      lines: 70,
       statements: -100,
     },
   },
